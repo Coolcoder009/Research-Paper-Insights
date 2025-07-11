ca86e0ba89634401290666267c8e7c157f9da383
Coolcoder009 <Coolcoder009>
2025-07-11 19:14:33 +0530

    feat:Chat interactions in chatbot API developed

---
+++ backend/app/services/chatbot_response.py
@@ -0,0 +1,15 @@
+from app.services.get_filetype import filetype
+from app.services.embedder import embeddings
+from app.services.retriever import retrieve_chunks
+from app.utils.prompt import CHATBOT_RESPONSE_PROMPT
+from app.services.llm_router import response_gemini
+
+def process(file, chatname):
+    extracted_contents, type = filetype(file)
+    embeddings(chatname, extracted_contents)
+    return {"Done!"}
+
+def response(query, chatname):
+    retrieve_content = retrieve_chunks(chatname, query)
+    prompt = CHATBOT_RESPONSE_PROMPT.format(context=retrieve_content, query=query)
+    return response_gemini(prompt)
